a2671f6c57e63fef250bc64ec5e3d86e5a1dbc90
ghostmander <jatinjindal.2003@gmail.com>
2024-02-07 13:54:34 +0530

    Types helper

---
+++ src/types.h
@@ -0,0 +1,14 @@
+#ifndef DISKUSAGEANALYZER_TYPES_H
+#define DISKUSAGEANALYZER_TYPES_H
+
+#define int_8 char
+#define int_16 short int
+#define int_32 int
+#define int_64 long long int
+
+#define uint_8 unsigned int_8
+#define uint_16 unsigned int_16
+#define uint_32 unsigned int_32
+#define uint_64 unsigned int_64
+
+#endif //DISKUSAGEANALYZER_TYPES_H
